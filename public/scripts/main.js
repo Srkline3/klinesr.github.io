df33b5b4abbeaabc32a1fbf3ac5a1de72abe0d04
Trey <srkline3@gmail.com>
2021-09-28 14:21:46 -0400

    ooof

---
+++ public/scripts/main.js
@@ -0,0 +1,11 @@
+new hoverEffect({
+	parent: document.getElementById("funny"),
+	intensity1: 1, 
+	intensity2: 2, 
+	speedIn: 1, 
+	speedOut: 1,
+	image1: "./images/Trey.jpg",
+	image2: "./images/me_silly.jpg",
+	displacementImage: "./images/displacement.jpg"
+})
+
